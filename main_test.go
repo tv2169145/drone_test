7dd318557c74f171149b88c73779894a5cf12b04
jimmy <tv2169145@gmail.com>
2020-08-29 03:56:12 +0800

    push 11

--- main_test.go
+++ main_test.go
@@ -4,7 +4,7 @@ import "testing"
 
 func TestGetHelloWorld(t *testing.T) {
 	result := GetHelloWorld()
-	if result != "hello jimmy 123456" {
+	if result != "hello jimmy 1234567" {
 		t.Fatal("should get hello jimmy")
 	}
 }
