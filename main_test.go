fa26f11c6d2560e81afb1771a9437cc339a6c1cd
jimmy <tv2169145@gmail.com>
2020-08-29 23:08:16 +0800

    test ci event

--- main_test.go
+++ main_test.go
@@ -4,7 +4,7 @@ import "testing"
 
 func TestGetHelloWorld(t *testing.T) {
 	result := GetHelloWorld()
-	if result != "hello jimmy 123456" {
+	if result != "hello jimmy 12345" {
 		t.Fatal("should get hello jimmy")
 	}
 }
